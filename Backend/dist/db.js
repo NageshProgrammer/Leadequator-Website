03ab4a1cfaecf7775c8430e0d843e08a9362488b
NageshProgrammer <139858065+NageshProgrammer@users.noreply.github.com>
2026-02-01 22:45:10 +0530

    fix: align backend TypeScript output with ESM for Render

--- Backend/dist/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-require("dotenv/config");
-const neon_http_1 = require("drizzle-orm/neon-http");
-const serverless_1 = require("@neondatabase/serverless");
-const sql = (0, serverless_1.neon)(process.env.DATABASE_URL);
-exports.db = (0, neon_http_1.drizzle)(sql);
