bfa85a29d5be2a9d2617edfe4528b2e9d12e2d4a
Narayan <88489426+narayan4256k@users.noreply.github.com>
2026-02-15 10:47:07 +0530

    small changes in pricing section

--- Frontend/src/[components]/plansection.tsx
+++ Frontend/src/[components]/plansection.tsx
@@ -292,7 +292,7 @@ export default function CongestedPricing() {
                      <LogIn className="w-5 h-5" />
                      {plan.name === "PILOT" 
                         ? "Start 14-Day Free Trial" 
-                        : "Start Free Trial"
+                        : "Subscribe To Scale"
                      }
                    </Link>
                 ) : (
